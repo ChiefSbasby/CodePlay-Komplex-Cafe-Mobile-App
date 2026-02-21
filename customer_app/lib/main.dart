2c3eb105c2cfb1c867806b2f4164f25b33ab26af
ChiefSbasby <sbasbbsbs@gmail.com>
2026-02-22 01:39:58 +0800

    Update main.dart

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -24,15 +24,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -40,27 +31,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
@@ -72,37 +46,82 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+
+              // banner with image
+              Row(
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Komplex Cafe"
+                        ),
+                        Text(
+                          "...Where every cup tells a story."
+                        )
+                      ],
+                    )
+                  ),
+                ],
+              ),
+
+              // short about info + link to menu
+              Row(
+                children: [
+                  Container(
+
+
+                  ),
+                ],
+              ),
+
+              // more info
+              Row(
+                children: [
+                  Container(
+
+
+                  ),
+                ],
+              ),
+
+              // opening hours
+              Row(
+                children: [
+                  Container(
+
+
+                  ),
+                ],
+              ),
+
+              // link to contacts
+              Row(
+                children: [
+                  Container(
+
+
+                  ),
+                ],
+              ),
+
+              // location info
+              Row(
+                children: [
+                  Container(
+
+
+                  ),
+                ],
+              ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
     );
   }
 }
